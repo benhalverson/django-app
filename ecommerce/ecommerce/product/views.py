4eb0294e4b15487e51453ddadfe13860718dbcef
Ben Halverson <benhalverson33@gmail.com>
2023-12-19 04:15:25 -0800

    add ability to add categories

--- ecommerce/ecommerce/product/views.py
+++ ecommerce/ecommerce/product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet ):
+    """
+    A simple Viewset for viewing all categories.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
