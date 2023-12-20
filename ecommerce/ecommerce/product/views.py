9428e078dc6187d6f5b9d416ebaffdd8c12c6793
Ben Halverson <benhalverson33@gmail.com>
2023-12-19 20:09:09 -0800

    added brand and product views

--- ecommerce/ecommerce/product/views.py
+++ ecommerce/ecommerce/product/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
 
 @extend_schema(responses={200: CategorySerializer})
@@ -11,5 +11,29 @@ class CategoryViewSet(viewsets.ModelViewSet ):
     """
     A simple Viewset for viewing all categories.
     """
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    try:
+        queryset = Category.objects.all()
+        serializer_class = CategorySerializer
+    except Exception as e:
+        print(e)
+
+@extend_schema(responses={200: BrandSerializer})
+class BrandViewSet(viewsets.ModelViewSet):
+    """
+    A simple Viewset for viewing all brands.
+    """
+    try:
+        queryset = Brand.objects.all()
+        serializer_class = BrandSerializer
+    except Exception as e:
+        print(e) 
+@extend_schema(responses={200: ProductSerializer})
+class ProductViewSet(viewsets.ModelViewSet ):
+    """
+    A simple Viewset for viewing all products.
+    """
+    try:
+        queryset = Product.objects.all()
+        serializer_class = ProductSerializer
+    except Exception as e:
+        print(e)
